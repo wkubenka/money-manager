2f9c25eb8ffb3e3c351c805292a4672f493173e7
William Kubenka <wkubenka@empire-medical.com>
2026-02-06 22:49:19 -0700

    Removed laravel splash page

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-})->name('home');
+})->middleware('guest')->name('home');
 
 Route::livewire('dashboard', 'pages::dashboard')
     ->middleware(['auth', 'verified'])
