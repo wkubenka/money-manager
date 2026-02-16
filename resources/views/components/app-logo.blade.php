8a534e948f9cd870e84327f4de716774eec5ebd6
William Kubenka <wkubenka@empire-medical.com>
2026-02-16 15:20:33 -0700

    Set name via config.

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -3,13 +3,13 @@
 ])
 
 @if($sidebar)
-    <flux:sidebar.brand name="Money Manager" {{ $attributes }}>
+    <flux:sidebar.brand :name="config('app.name')" {{ $attributes }}>
         <x-slot name="logo" class="flex aspect-square size-8 items-center justify-center rounded-md bg-emerald-500">
             <x-app-logo-icon class="size-5 fill-current text-white" />
         </x-slot>
     </flux:sidebar.brand>
 @else
-    <flux:brand name="Money Manager" {{ $attributes }}>
+    <flux:brand :name="config('app.name')" {{ $attributes }}>
         <x-slot name="logo" class="flex aspect-square size-8 items-center justify-center rounded-md bg-emerald-500">
             <x-app-logo-icon class="size-5 fill-current text-white" />
         </x-slot>
