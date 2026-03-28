d4bbd95cbd8f7818578235339f24c44b9d20e567
William Kubenka <wkubenka@empire-medical.com>
2026-03-28 11:47:26 -0700

    Add debt payoff route and empty page scaffold

---
+++ resources/views/pages/debt-payoff/⚡index.blade.php
@@ -0,0 +1,13 @@
+<?php
+
+use Livewire\Component;
+
+new class extends Component {
+    //
+}; ?>
+
+<section class="w-full">
+    <x-page-heading title="Debt Payoff" subtitle="Plan your path to debt freedom" />
+
+    <p class="text-sm text-zinc-500 dark:text-zinc-400">Coming soon.</p>
+</section>
---
+++ routes/debt-payoff.php
@@ -0,0 +1,6 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::livewire('debt-payoff', 'pages::debt-payoff.index')
+    ->name('debt-payoff.index');
